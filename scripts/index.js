4cf157c331f7ecdd2dd1bdd9b48956aac2f56c04
LiaJahan <jesmoonlia@gmail.com>
2025-10-21 19:41:55 +0600

    working on js

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,12 @@ const fetchFile = () =>
     .then(json => loadFile(json.data)) 
 };
 
-fetchFile();
+const removeActive = () => {
+  const lessonBtnRemoveActive = document.querySelectorAll('.lesson-btn-remove-active');
+  lessonBtnRemoveActive.forEach(btn => btn.classList.remove('active'));
+};
+
+
 const loadFile = (lessons) => 
     
     {
@@ -23,7 +28,7 @@ const loadFile = (lessons) =>
             const lessonButton = document.createElement('div');
             lessonButton.innerHTML =
             `
-            <button class="btn btn-outline btn-primary"><i
+            <button onclick='loadLevelWord(${lesson.level_no})' id='lesson-btn-${lesson.level_no}' class="btn btn-outline btn-primary lesson-btn-remove-active"><i
                                 class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
             `
             divContainer.appendChild(lessonButton)
@@ -32,3 +37,62 @@ const loadFile = (lessons) =>
         // console.log(lessons)
     
     };
+
+    // if lessson button is clicked it will show the wordlists
+    const loadLevelWord=(id)=>
+    {
+        const url = `https://openapi.programming-hero.com/api/level/${id}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(json=>
+            {
+                removeActive();
+                const clickbtn2 = document.getElementById(`lesson-btn-${id}`)
+                clickbtn2.classList.add('active');
+                displayWords(json.data)
+            }
+        )
+    }
+
+    const displayWords = (words)=>{
+        const wordContainer = document.getElementById('word-container');
+        wordContainer.innerHTML = '';
+
+        if(words.length == 0)
+        {
+            wordContainer.innerHTML = `
+            <div class="font-bangla col-span-full text-center space-y-5">
+                <img class="mx-auto" src="./assets/alert-error.png" alt="">
+                <p class="font-normal text-sm">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="font-medium text-3xl">নেক্সট Lesson এ যান</h2>
+            </div>
+
+            `;
+            return;
+        }
+        words.forEach(word=>
+        {
+            // dynamic word box
+            const wordBox = document.createElement('div');
+            wordBox.innerHTML=
+            `
+            
+             <div class="text-center rounded-md shadow-lg bg-white p-10 space-y-5">
+                <h1 class="text-3xl font-bold">${word.word ? word.word:'শব্দ পাওয়া যায়নি'}</h1>
+                
+                <p class="font-medium text-lg">Meaning /Pronounciation</p>
+                
+                <p class="font-bangla font-semibold text-2xl">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'উচ্চারণ পাওয়া যায়নি' }" </p>
+                
+                <div class="flex items-center justify-between">
+                    <button onclick="my_modal_5.showModal()" class="bg-blue-100 hover:bg-blue-200 rounded-md p-2"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="bg-blue-100 hover:bg-blue-200 rounded-md p-2"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+             </div>
+            `
+            wordContainer.append(wordBox);
+        })
+        
+    }
+
+fetchFile();
